3959a59d576d90c1ce19f86e0d4a078a3f40759e
elroy404 <sdzgrail@gmail.com>
2016-07-28 17:19:18 -0700

    7/28 v1

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 
 $(document).ready(function(){
-	var randomNum;
-	var guessNum;
-	numGenerator();
+	var randomNum,
+	guessNum,
+	numFeedback;
 
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-  	});
+	var alreadyGuessed = [];
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+
+	pageLoad();
+
+		function pageLoad(){
+			/*--- Display information modal box ---*/
+	  	$(".what").click(function(){
+	    	$(".overlay").fadeIn(1000);
+	  	});
+
+	  	/*--- Hide information modal box ---*/
+	  	$("a.close").click(function(){
+	  		$(".overlay").fadeOut(1000);
+	  	});
+			numGenerator();
+		}
 
 		// Generates a random number between 1-100
 		function numGenerator(){
@@ -20,40 +28,64 @@ $(document).ready(function(){
 			console.log("random #: " + randomNum);
 		}
 
+		//click NEW GAME button to generate a new random number
+		$(".new").click(function(){
+			numGenerator();
+			alreadyGuessed = [];
+			numFeedback = "";
+			$("#feedback").text("Make your Guess!");
+		});
+
 		// stores users guess
-		$("#guessButton").click(function(){
+		$("#guessButton").click(function(event){
+			event.preventDefault();
 	    guessNum = parseInt($("#userGuess").val());
-			feedback(randomNum, guessNum);
+			numCheck(randomNum, guessNum);
+			$("#userGuess").val("");
+			guessQ(guessNum);
+			guessCount =+ 1;
 	  });
 
+		function buttonCheck(){
+			alert("please enter your guess");
+		}
+
 		// checks number and outputs feedback
-		function feedback(rand, guess){
-			var x = guess - rand;
-			var i = Math.abs(x);
-			console.log(i);
+		function numCheck(rand, guess){
+			var howClose = guess - rand;
+			numFeedback = Math.abs(howClose);
+			console.log(numFeedback);
 			console.log("random #: " + rand);
 			console.log("guess #: " + guess);
-			if(i>50){
-				console.log("ice cold");
+			if(numFeedback >= 50){
+				$("#feedback").text("ice cold");
 			}
-			else if(i>=40 && i<50){
-				console.log("cold");
+			else if(numFeedback >= 40 && numFeedback < 50){
+				$("#feedback").text("cold");
 			}
-			else if(i>=20 && i<40){
-				console.log("warm");
+			else if(numFeedback >= 20 && numFeedback < 40){
+				$("#feedback").text("warm");
 			}
-			else if(i>=10 && i<20){
-				console.log("hot");
+			else if(numFeedback >= 10 && numFeedback < 20){
+				$("#feedback").text("hot");
 			}
-			else if(i>=1 && i<10){
-				console.log("very hot");
+			else if(numFeedback >= 1 && numFeedback < 10){
+				$("#feedback").text("very hot");
 			}
-			else if(i==0){
-				console.log("This is the number!");
+			else if(numFeedback == 0){
+				$("#feedback").text("This is the number! \nClick new game to play again");
 			}
 			else{
 				console.log("Error");
 			}
+		}
+
+		function guessQ(guess){
+			var guessCount = 0;
+			alreadyGuessed.push(guess);
+			$("#guessList").append("<li>" + alreadyGuessed[guessCount] + "</li>");
+			console.log(guessCount);
+			console.log(alreadyGuessed[guessCount]);
 		}
 
 });
